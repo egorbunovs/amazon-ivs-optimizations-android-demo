059aeae8db2852313f4fc81466c030802e786773
Eddy Gorbunov <edijs.gorbunovs@testdevlab.com>
2025-06-11 16:46:53 +0300

    Updated libs;

--- app/src/main/java/com/amazon/ivs/optimizations/common/LineNumberDebugTree.kt
+++ app/src/main/java/com/amazon/ivs/optimizations/common/LineNumberDebugTree.kt
@@ -5,7 +5,14 @@ import timber.log.Timber
 private const val TIMBER_TAG = "IVS Best Practices"
 
 class LineNumberDebugTree : Timber.DebugTree() {
+    private var method = ""
 
-    override fun createStackElementTag(element: StackTraceElement) =
-        "$TIMBER_TAG: (${element.fileName}:${element.lineNumber}) #${element.methodName} "
+    override fun createStackElementTag(element: StackTraceElement): String {
+        method = "#${element.methodName}"
+        return "(${element.fileName}:${element.lineNumber})"
+    }
+
+    override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+        super.log(priority, tag, "${TIMBER_TAG}: $method: $message", t)
+    }
 }
